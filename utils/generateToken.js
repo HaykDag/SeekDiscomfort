9b9430dedaf76e6f0f66e3fd3a3a2e06f838a9c7
HaykDag <106971157+HaykDag@users.noreply.github.com>
2023-09-20 14:43:25 +0800

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, username) =>{
     res.cookie('access_token',token,{
         httpOnly:true,
         secure: process.env.NODE_ENV !== 'development',
-        SameSite: 'None',
+        sameSite: 'None',
         maxAge: 86400000 //1day in miliseconds
     })
     
