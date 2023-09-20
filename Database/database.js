8447a97fcc96dd3821f09acf3989a7ed4fa09684
HaykDag <106971157+HaykDag@users.noreply.github.com>
2023-09-20 14:27:56 +0800

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.SQL_HOST',
+    host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE
